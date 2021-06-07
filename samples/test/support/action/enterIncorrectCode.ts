610d6ab8332c2b25281a31f77ea2913bb17c9e85
denys.oblohin <denys.oblohin@okta.om>
2021-06-07 22:10:04 +0300

    fix

--- samples/test/support/action/enterIncorrectCode.ts
+++ samples/test/support/action/enterIncorrectCode.ts
@@ -3,7 +3,7 @@ import setInputField from './setInputField';
 import clickElement from './clickElement';
 
 export default async function () {
-  const code = '!incorrect!';
+  const code = '000000';
   await setInputField('set', code, ChallengeAuthenticator.code);
   await clickElement('click', 'selector', ChallengeAuthenticator.submit);
 }
