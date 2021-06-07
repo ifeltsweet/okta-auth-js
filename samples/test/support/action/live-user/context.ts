fc347a13b5976ed21b45f6cbba119f28570a6f3b
denys.oblohin <denys.oblohin@okta.om>
2021-06-07 22:00:17 +0300

    lint fix lint fix f

--- samples/test/support/action/live-user/context.ts
+++ samples/test/support/action/live-user/context.ts
@@ -1,7 +1,9 @@
 import { User } from '@okta/okta-sdk-nodejs';
 import {A18nProfile} from '../../management-api/a18nClient';
 
-export default interface ActionContext {
+interface ActionContext {
   a18nProfile: A18nProfile;
   user: User;
-};
+}
+
+export default ActionContext;
